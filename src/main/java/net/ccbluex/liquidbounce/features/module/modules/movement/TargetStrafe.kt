a86def84b0ae93b39829f5350fe66aa0191945a6
Nerek23 <knesszaz9@gmail.com>
2023-05-17 11:14:00 +0200

    killaura fixed not detecting targets strafe needs to be fixed

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/TargetStrafe.kt
@@ -1,6 +1,7 @@
 // LiquidX Development by PrahXZ and Haflin with FDP Base modified. v2.0 R1
 package net.ccbluex.liquidbounce.features.module.modules.movement
 
+import jdk.nashorn.internal.runtime.Debug
 import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Module
@@ -10,74 +11,132 @@ import net.ccbluex.liquidbounce.features.module.modules.combat.KillAura
 import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.features.value.*
+import net.ccbluex.liquidbounce.utils.ClientUtils
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityLivingBase
 import net.minecraft.util.AxisAlignedBB
 import net.minecraft.util.MathHelper
 import org.lwjgl.input.Keyboard
-import net.minecraft.util.Vec3;
 
 @ModuleInfo(name = "TargetStrafe",  category = ModuleCategory.MOVEMENT)
 class TargetStrafe : Module() {
-    val strafeRange = FloatValue("Range", 2.0f, 0.1f, 4.0f) // esto es para saber a que distancia del jugador se calculara la direccion
-    private val Jump = BoolValue("Jump", true) // Esto es para que no te caigas al vacio
-    private val safeWalk = BoolValue("SafeWalk", true) // Esto es para que no te caigas al vacio
-    private val moveSpeed = FloatValue("MoveSpeed", 0.5f, 0.1f, 2.0f)
+    val range = FloatValue("Range", 2.0f, 0.1f, 4.0f).displayable { !behind.get() }
+    private val modeValue = ListValue("KeyMode", arrayOf("Jump", "None"), "Jump")
+    private val safewalk = BoolValue("SafeWalk", true)
+    val behind = BoolValue("Behind", false)
+    val thirdPerson = BoolValue("ThirdPerson", false)
     val killAura = LiquidBounce.moduleManager.getModule(KillAura::class.java)
+    val speed = LiquidBounce.moduleManager.getModule(Speed::class.java)
+    val longJump = LiquidBounce.moduleManager.getModule(LongJump::class.java)
+    val flight = LiquidBounce.moduleManager.getModule(Fly::class.java)
+
+    var direction = 1
+    var lastView = 0
+    var hasChangedThirdPerson = true
+
+    override fun onEnable() {
+        hasChangedThirdPerson = true
+        lastView = mc.gameSettings.thirdPersonView
+    }
+
     @EventTarget
     fun onMotion(event: MotionEvent) {
-        if (event.eventState == EventState.PRE) {
-            if (Jump.get() && !mc.gameSettings.keyBindJump.isKeyDown()) {
-                return
-            }
-            val target = getTarget() ?: return
 
-            //   val speed = getSpeed(direction)
-            if (safeWalk.get() && checkVoid()) {
-                return
-
-                // Desactivar todas las teclas a w d s
+        if (thirdPerson.get()) { // smart change back lol
+            if (getTarget() != null) {
+                if (hasChangedThirdPerson) lastView = mc.gameSettings.thirdPersonView
+                mc.gameSettings.thirdPersonView = 1
+                hasChangedThirdPerson = false
+            } else if (!hasChangedThirdPerson) {
+                mc.gameSettings.thirdPersonView = lastView
+                hasChangedThirdPerson = true
             }
-            if(killAura?.target != null) {
-                val angle = Math.toRadians(mc.thePlayer.rotationYaw.toDouble())
-                val x = Math.cos(angle) * strafeRange.get() + target.posX
-                val z = Math.sin(angle) * strafeRange.get() + target.posZ
-                val y = target.posY
-                val newTarget = Vec3(x, y, z)
-                val newDirection = newTarget.subtract(mc.thePlayer.getPositionEyes(1.0f)).normalize()
-                mc.thePlayer.motionX = newDirection.xCoord * moveSpeed.get()
-                mc.thePlayer.motionZ = newDirection.zCoord * moveSpeed.get()
-            }
-
         }
+
+        if (event.eventState == EventState.PRE) {
+            if (mc.thePlayer.isCollidedHorizontally || safewalk.get() && checkVoid() && !flight!!.state)
+                this.direction = -this.direction
+        }
+    }
+
+    @EventTarget
+    fun onMove(event: MoveEvent) {
+
+        if (getTarget() != null) {
+       //     ClientUtils.logError("Debug " + getTarget())
+            strafe(event, MovementUtils.getSpeed().toInt())
+            ClientUtils.logError("Debug " + strafe(event, MovementUtils.getSpeed().toInt()))
+            if (safewalk.get() && checkVoid() && !flight!!.state)
+                event.isSafeWalk = true
+        }
+        /*
+        if(LiquidBounce.moduleManager[KillAura::class.java]!!.state) {
+            ClientUtils.displayChatMessage("Debug funcionas mierda?")
+        }
+        */
+
+
     }
 
     private fun getTarget(): EntityLivingBase? {
         var target: EntityLivingBase? = null
         var distance = Float.MAX_VALUE
-        for (entity in mc.theWorld.loadedEntityList) {
-            if (entity is EntityLivingBase && entity != mc.thePlayer && entity.isEntityAlive && !entity.isInvisible) {
-                val currentDistance = mc.thePlayer.getDistanceToEntity(entity)
-                if (currentDistance < distance && currentDistance <= strafeRange.get()) {
-                    target = entity
-                    distance = currentDistance
+        if(LiquidBounce.moduleManager[KillAura::class.java]!!.hitable) {
+            for (entity in mc.theWorld.loadedEntityList) {
+                if (entity is EntityLivingBase && entity != mc.thePlayer && entity.isEntityAlive && !entity.isInvisible) {
+                    val currentDistance = mc.thePlayer.getDistanceToEntity(entity)
+                    if (currentDistance < distance && currentDistance <= range.get()) {
+                        target = entity
+                        distance = currentDistance
+                    }
                 }
             }
         }
+
+
+
+
         return target
     }
 
-    private fun getDirection(target: EntityLivingBase): Vec3? {
-        val x = target.posX - mc.thePlayer.posX
-        val z = target.posZ - mc.thePlayer.posZ
-        val direction = Vec3(x, 0.0, z).normalize()
-        return if (direction.xCoord.isNaN() || direction.zCoord.isNaN()) null else direction
+    fun strafe(event: MoveEvent, moveSpeed: Int) {
+        val target = getTarget()
+
+        val rotYaw = RotationUtils.getRotationsEntity(target!!).yaw
+
+        if (mc.thePlayer.getDistanceToEntity(target) <= 1.5)
+            MovementUtils.setSpeed(event, moveSpeed, rotYaw, direction.toDouble(), 0.0)
+        else
+            MovementUtils.setSpeed(event, moveSpeed, rotYaw, direction.toDouble(), 1.0)
+
+        if (behind.get()) {
+            val xPos: Double = target.posX + -Math.sin(Math.toRadians(target.rotationYaw.toDouble())) * -2
+            val zPos: Double = target.posZ + Math.cos(Math.toRadians(target.rotationYaw.toDouble())) * -2
+            event.x = ((moveSpeed * -MathHelper.sin(
+                Math.toRadians(RotationUtils.getRotations1(xPos, target.posY, zPos)[0].toDouble())
+                    .toFloat()
+            )).toDouble())
+            event.z = ((moveSpeed * MathHelper.cos(
+                Math.toRadians(RotationUtils.getRotations1(xPos, target.posY, zPos)[0].toDouble())
+                    .toFloat()
+            )).toDouble())
+        } else {
+            if (mc.thePlayer.getDistanceToEntity(target) <= range.get())
+                MovementUtils.setSpeed(event, moveSpeed, rotYaw, direction.toDouble(), 0.0)
+            else
+                MovementUtils.setSpeed(event, moveSpeed, rotYaw, direction.toDouble(), 1.0)
+        }
     }
-    /*
-     private fun getSpeed(direction: Vec3?): Double {
-         return if (direction == null) 0.0 else mc.thePlayer.getDistanceToEntity(mc.thePlayer.ridingEntity ?: mc.thePlayer) * 0.15
-     }
-     */
+
+    val keyMode: Boolean
+        get() = when (modeValue.get()) {
+            "Jump" -> Keyboard.isKeyDown(Keyboard.KEY_SPACE)
+            "None" -> mc.thePlayer.movementInput.moveStrafe != 0f || mc.thePlayer.movementInput.moveForward != 0f
+            else -> false
+        }
+
+
+
     private fun checkVoid(): Boolean {
         for (x in -1..0) {
             for (z in -1..0) {
@@ -89,14 +148,14 @@ class TargetStrafe : Module() {
         return false
     }
 
-    private fun isVoid(x: Int, z: Int): Boolean {
+    private fun isVoid(X: Int, Z: Int): Boolean {
         if (mc.thePlayer.posY < 0.0) {
             return true
         }
         var off = 0
         while (off < mc.thePlayer.posY.toInt() + 2) {
-            val bb = mc.thePlayer.entityBoundingBox.offset(x.toDouble(), (-off).toDouble(), z.toDouble())
-            if (mc.theWorld!!.getCollidingBoundingBoxes(mc.thePlayer, bb).isEmpty()) {
+            val bb: AxisAlignedBB = mc.thePlayer.entityBoundingBox.offset(X.toDouble(), (-off).toDouble(), Z.toDouble())
+            if (mc.theWorld!!.getCollidingBoundingBoxes(mc.thePlayer as Entity, bb).isEmpty()) {
                 off += 2
                 continue
             }
@@ -105,7 +164,4 @@ class TargetStrafe : Module() {
         }
         return true
     }
-
 }
-
-
